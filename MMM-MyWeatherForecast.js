7e82bc01fe97dc5964917d43f060dc82eb62c9cc
Harm Tilburgs <harm@tilburgs.com>
2026-01-24 22:01:30 +0100

    Update MMM-MyWeatherForecast.js

--- MMM-MyWeatherForecast.js
+++ MMM-MyWeatherForecast.js
@@ -110,7 +110,7 @@ Module.register("MMM-MyWeatherForecast", {
                 "rain": "rain.svg",
                 "snow": "snow.svg",
                 "thunderstorm": "thunderstorm.svg",
-                "mist": "fog.svg"
+                "mist": "mist.svg"
             }
         };
     },

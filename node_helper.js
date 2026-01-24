38e697d0531eb6b915b4ff64e0967880a01cbabb
Harm Tilburgs <harm@tilburgs.com>
2026-01-24 11:06:29 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -3,7 +3,7 @@ const NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
     start: function() {
         console.log("MMM-MyWeatherForecast helper started...");
-        this.cache = null; // store last successful weather data
+        this.cache = null;
     },
 
     socketNotificationReceived: async function(notification, payload) {
@@ -12,17 +12,13 @@ module.exports = NodeHelper.create({
             const url = `https://api.piratesky.com/weather?lat=${latitude}&lon=${longitude}&apikey=${apiKey}&lang=${lang}`;
 
             try {
-                const response = await fetch(url);  // native Node 18+ fetch
+                const response = await fetch(url); // native fetch
                 const data = await response.json();
 
-                // cache successful result
-                this.cache = data;
-
+                this.cache = data; // store last successful fetch
                 this.sendSocketNotification("WEATHER_RESULT", data);
             } catch (err) {
                 console.error("MMM-MyWeatherForecast fetch error:", err);
-
-                // if fetch fails, send cached data if available
                 if (this.cache) {
                     console.warn("Sending cached weather data due to API failure.");
                     this.sendSocketNotification("WEATHER_RESULT", this.cache);

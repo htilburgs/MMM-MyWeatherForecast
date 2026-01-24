8ec9f1bb3f67ca8e0e20cb493971ae2dd11c2968
Harm Tilburgs <harm@tilburgs.com>
2026-01-24 12:43:36 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -10,25 +10,17 @@ module.exports = NodeHelper.create({
         if (notification === "FETCH_WEATHER") {
             const { apiKey, userlat, userlon, units, lang } = payload;
             const url = `https://api.pirateweather.net/forecast/${apiKey}/${userlat},${userlon}?units=${units}&lang=${lang}`;
-
             console.log("[MMM-MyWeatherForecast] Fetching URL:", url);
 
             try {
                 const response = await fetch(url);
-                console.log("[MMM-MyWeatherForecast] Response status:", response.status);
-
                 if (!response.ok) throw new Error(`HTTP error ${response.status}`);
                 const data = await response.json();
-
-                console.log("[MMM-MyWeatherForecast] Weather data received:", data);
-
                 this.cache = data;
                 this.sendSocketNotification("WEATHER_RESULT", data);
             } catch (err) {
                 console.error("[MMM-MyWeatherForecast] Fetch error:", err);
-
                 if (this.cache) {
-                    console.warn("[MMM-MyWeatherForecast] Sending cached weather data.");
                     this.sendSocketNotification("WEATHER_RESULT", this.cache);
                 } else {
                     this.sendSocketNotification("WEATHER_ERROR", err.message);

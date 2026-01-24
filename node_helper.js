8a76929046670066cdf878bd831f46793bc280db
Harm Tilburgs <harm@tilburgs.com>
2026-01-24 10:30:15 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -4,6 +4,7 @@ const fetch = require("node-fetch");
 module.exports = NodeHelper.create({
     start: function() {
         console.log("MMM-MyWeatherForecast helper started...");
+        this.cache = null; // Store last successful weather data
     },
 
     socketNotificationReceived: async function(notification, payload) {
@@ -15,10 +16,21 @@ module.exports = NodeHelper.create({
             try {
                 const response = await fetch(url);
                 const data = await response.json();
+
+                // Cache the last successful data
+                this.cache = data;
+
                 this.sendSocketNotification("WEATHER_RESULT", data);
             } catch (err) {
                 console.error("MMM-MyWeatherForecast fetch error:", err);
-                this.sendSocketNotification("WEATHER_ERROR", err.message);
+
+                // If fetch fails, send cached data if available
+                if (this.cache) {
+                    console.warn("Sending cached weather data due to API failure.");
+                    this.sendSocketNotification("WEATHER_RESULT", this.cache);
+                } else {
+                    this.sendSocketNotification("WEATHER_ERROR", err.message);
+                }
             }
         }
     }
